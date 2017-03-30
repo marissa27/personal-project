ad7fcaf1a1b5256036f1a0c1bda6aecc5fa2a48c
Michael Fenwick <mickyfen17@aol.com>
2017-03-30 17:02:27 -0600

    assign signedIn boolean to user object

--- app/reducers/userReducer.js
+++ app/reducers/userReducer.js
@@ -2,12 +2,13 @@ const initialState = {
   name: null,
   email: null,
   id: null,
+  signedIn: false
 }
 
 const user = (state=initialState, action) => {
   switch(action.type) {
     case 'SIGN_IN':
-    return Object.assign({}, state, action.user);
+    return Object.assign({}, state, action.user, { signedIn: true });
     break;
   default:
     return state;
