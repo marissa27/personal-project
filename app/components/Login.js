103634945e7aa1ec3d7469abe9c5842f8cc6c968
Michael Fenwick <mickyfen17@aol.com>
2017-03-30 11:01:41 -0600

    add state to login and update values with onChange of inputs

--- app/components/Login.js
+++ app/components/Login.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Login = () => {
-  return(
-    <div>
-      <h2>Login Page</h2>
-      <form>
-        Email
-        <input type="text" name="email" value="Email"/>
-        Password
-        <input type="text" name="password" value="Password"/>
+export default class Login extends Component {
+  constructor() {
+    super()
+    this.state = {
+      email: '',
+      password: '',
+      error: ''
+    }
+  }
 
-        <input type="submit" value="Submit"/>
-      </form>
-    </div>
-  );
+  handleUserInput(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  render() {
+    return(
+      <div>
+        <h2>Login Page</h2>
+        <form>
+          Email
+          <input
+            type="text"
+            name="email"
+            value={this.state.email}
+            onChange={ (e) => this.handleUserInput(e) }
+          />
+          Password
+          <input
+            type="text"
+            name="password"
+            value={this.state.password}
+            onChange={ (e) => this.handleUserInput(e) }
+          />
+
+          <input
+            type="submit"
+            value="Submit"
+          />
+        </form>
+      </div>
+    );
+  }
 }
-
-export default Login;
