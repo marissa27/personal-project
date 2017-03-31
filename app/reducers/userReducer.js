9ff9f9fd626c2e77f96ce7d53a4be18c6c85475c
Michael Fenwick <mickyfen17@aol.com>
2017-03-30 19:44:55 -0600

    add signout switch case

--- app/reducers/userReducer.js
+++ app/reducers/userReducer.js
@@ -8,8 +8,11 @@ const initialState = {
 const user = (state=initialState, action) => {
   switch(action.type) {
     case 'SIGN_IN':
-    return Object.assign({}, state, action.user, { signedIn: true });
-    break;
+      return Object.assign({}, state, action.user, { signedIn: true });
+      break;
+    case 'SIGN_OUT':
+      return Object.assign({}, initialState);
+      break;
   default:
     return state;
   }
