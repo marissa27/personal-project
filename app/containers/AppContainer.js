6fc4a15307a23b8aa5c8a671f9c656ba32e7170a
Michael Fenwick <mickyfen17@aol.com>
2017-04-07 19:37:40 -0600

    import removeFavorite and pass as prop to App

--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 
 import App from '../components/App';
-import { fetchMovies, signOut, showFavorites, addFavorite } from '../actions/actions';
+import { fetchMovies, signOut, showFavorites, addFavorite, removeFavorite } from '../actions/actions';
 
 const mapStateToProps = (state) => {
   return state
@@ -12,7 +12,10 @@ const mapDispatchToProps = (dispatch) => {
     fetchMovies: () => dispatch(fetchMovies()),
     signOut: () => dispatch(signOut()),
     showFavorites: (favorites) => dispatch(showFavorites(favorites)),
-    addFavorite: (movie_id, userID, title, poster_path, release_date, vote_average, overview) => dispatch(addFavorite(movie_id, userID, title, poster_path, release_date, vote_average, overview))
+    addFavorite: (movie_id, userID, title, poster_path, release_date, vote_average, overview) => {
+      dispatch(addFavorite(movie_id, userID, title, poster_path, release_date, vote_average, overview))
+    },
+    removeFavorite: (movie_id, userID) => dispatch(removeFavorite(movie_id, userID))
   }
 }
 
